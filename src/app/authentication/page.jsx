d78c622f2f16df6ec273ede82b988af26a2b668a
swarties <76826079+swarties@users.noreply.github.com>
2025-11-20 21:36:37 -0500

    added a button to log in

--- src/app/authentication/page.jsx
+++ src/app/authentication/page.jsx
@@ -1,8 +1,17 @@
+'use server';
+
 import { supabase } from "@/lib/supabaseClient";
-import Button from "@/components/ui/button";
+import { Button } from "@/components/ui/button";
 
 const gitHubLoginHandler = async () => {
     const { error } = await supabase.auth.signInWithOAuth({provider : 'github'});
     if (error) console.log(error.message) 
 }
 
+function ButtonOutline() {
+    return <Button variant="outline">Login</Button>
+}
+
+export default function Auth() {
+    return <ButtonOutline onClick={gitHubLoginHandler}></ButtonOutline>
+}
