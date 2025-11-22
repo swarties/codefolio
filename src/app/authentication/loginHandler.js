ccdb63489cd0b84c41fcc9e9f292f0011137358a
swarties <76826079+swarties@users.noreply.github.com>
2025-11-21 20:47:23 -0500

    trying to fix localhost redirect

--- src/app/authentication/loginHandler.js
+++ src/app/authentication/loginHandler.js
@@ -1,6 +1,11 @@
 import { supabase } from "@/lib/supabaseClient";
 const gitHubLoginHandler = async () => {
-  const { error } = await supabase.auth.signInWithOAuth({ provider: "github" });
+  const { error } = await supabase.auth.signInWithOAuth({
+    provider: "github",
+    options: {
+      redirectTo: "../"
+    },
+  });
   if (error) console.log(error.message);
 };
 
