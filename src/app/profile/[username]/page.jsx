e4fd5f354508d97888ff3cb96797021181e7beea
swarties <76826079+swarties@users.noreply.github.com>
2025-11-26 12:15:42 -0500

    put back data fetching into the dynamic routing and ameliorated fetching logic

--- src/app/profile/[username]/page.jsx
+++ src/app/profile/[username]/page.jsx
@@ -1,14 +1,66 @@
-import { supabaseAdmin } from "@/lib/supabaseAdmin";
 import fetchUserData from "./userDataFetcher";
+import { supabaseAdmin } from "@/lib/supabaseAdmin";
 
 export default async function Page({ params }) {
   const { username } = await params;
-  
-  var debugVal = await searchUsername(username);
-  console.log("username exists?", debugVal);
+  async function CheckUserandFetchData() {
+    let dUE = false;
+    const { data, error } = await supabaseAdmin
+      .from("profiles")
+      .select("*")
+      .eq("username", username);
+
+    if (error) {
+      console.log("Error contacting database:", error.message || error);
+      return { dUE, data: "nodata" };
+    }
+    if (Array.isArray(data) && data.length > 0) {
+      dUE = true;
+      return { dUE, data };
+    } else {
+      return { dUE, data };
+    }
+  }
+
+  const userInfo = await CheckUserandFetchData();
+
+  if (!dUE) {
+    
+  }
+
+  console.log(userInfo);
+
   return (
     <div>
       <p>{username}</p>
     </div>
   );
 }
+
+/* const pageUsername = await params;
+let doesUsernameExist = false;
+let userData = {};
+    (async () => {
+      let dUE = false;
+      const { data, error } = await supabaseAdmin
+        .from("profiles")
+        .select("*")
+        .eq("username", pageUsername);
+  
+      if (error) {
+        console.error(
+          "Error checking username in database:",
+          error.message || error
+        );
+        dUE = false;
+      }
+      
+      dUE = Array.isArray(data) && data.length > 0; //do this with an if to return either a value to if later for no username found page or a dataset
+      doesUsernameExist = dUE;
+  
+      if (doesUsernameExist) {
+          userData = data;
+      } else {
+          return false;
+      }
+  })(); */
