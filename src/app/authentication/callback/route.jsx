6e7c2110ec92901638bf8fa641800936a808f643
swarties <76826079+swarties@users.noreply.github.com>
2025-11-28 01:37:58 -0500

    callback rewrite into server route

---
+++ src/app/authentication/callback/route.jsx
@@ -0,0 +1,44 @@
+import { createClient } from "@/lib/supabase/server";
+import { NextResponse } from "next/server";
+
+export default async function GET(request) {
+  const { searchParams, origin } = new URL(request.URL);
+  const code = searchParams.get("code");
+  const next = searchParams.get("next") ?? "/dashboard";
+
+  if (code) {
+    const supabase = await createClient();
+
+    const { data, error } = await supabase.auth.exchangeCodeForSession(code);
+
+    if (!error && data?.user) {
+      const { user } = data;
+      const { user_metadata } = user;
+
+      const { error: dbError } = await supabase.from("profiles").upsert(
+        [
+          {
+            github_id: user_metadata?.sub,
+            username: user_metadata?.user_name,
+            auth_user_id: user.id,
+            avatar_url: user_metadata?.avatar_url,
+          },
+        ],
+        { onConflict: "github_id" }
+      );
+
+      if (dbError) {
+        console.error(
+          "Error upserting data into database:",
+          error.message || error
+        );
+      }
+
+      return NextResponse.redirect(`${origin}${next}`);
+    }
+  }
+
+  return NextResponse.redirect(
+    `${origin}/authentication?error=auth_code_error`
+  );
+}
