e30afac3c5bd0692c91c2712e3ab6bccb7899dbd
swarties <76826079+swarties@users.noreply.github.com>
2025-12-02 19:50:09 -0500

    starting styling of the profile dynamic page

--- src/app/profile/[username]/page.jsx
+++ src/app/profile/[username]/page.jsx
@@ -15,20 +15,18 @@ async function Repos({ repo_option, uID }) {
 
   if (repoOption) {
     // latest 5 repos
-    text = "5 latest repositories"
+    text = "5 latest repositories";
     repos = await lastGetter(user_id);
   } else {
     // top 5 starred repos
-    text = "5 most starred repositories"
+    text = "5 most starred repositories";
     repos = await starGetter(user_id);
   }
   // display logic here with the var repos
 
   return (
     <ul>
-      <br />
-      <h1 style={{fontSize:"25px"}}>{text}</h1>
-      <br />
+      <h1 style={{ fontSize: "25px" }}>{text}</h1>
       <hr />
       {repos.map((repo, index) => (
         <li key={index}>
@@ -37,7 +35,7 @@ async function Repos({ repo_option, uID }) {
           </a>
           <p>{repo[1]}</p>
           <br></br>
-          <hr ></hr>
+          <hr></hr>
         </li>
       ))}
     </ul>
@@ -66,7 +64,9 @@ async function CheckUserandFetchData(username) {
 export default async function Page({ params }) {
   const { username } = await params;
   const serverAns = await CheckUserandFetchData(username);
-  const serverData = (serverAns.dUE) ? await serverAns.data[0] : {github_id: false};
+  const serverData = serverAns.dUE
+    ? await serverAns.data[0]
+    : { github_id: false };
   const userID = serverData.github_id;
 
   if (!serverAns.dUE) {
@@ -84,15 +84,29 @@ export default async function Page({ params }) {
 
   return (
     <>
-      <div>
-        <p>{userData.username}</p>
-        <p>Bio : {userData.bio}</p>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
         <Image
           src={userData.avatar_url}
           alt="User Avatar"
           width={100}
           height={100}
+          style={{
+            borderRadius: "50%",
+            marginTop: "2em",
+            borderWidth: "1px",
+            borderColor: "black",
+            borderStyle: "solid",
+          }}
         ></Image>
+        <p style={{marginBottom:"1em"}}>{userData.username}</p>
+        <p>Bio : {userData.bio}</p>
 
         <Suspense
           fallback={
