8303a9c26fc95e7b3158580ae13a12ad440609d1
swarties <76826079+swarties@users.noreply.github.com>
2025-12-03 21:46:21 -0500

    fixed svg displacement

--- src/app/profile/[username]/page.jsx
+++ src/app/profile/[username]/page.jsx
@@ -5,23 +5,25 @@ import { Suspense } from "react";
 import React from "react";
 
 function RedirectSVG() {
-  return <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width={16}
-                height={16}
-                fill="currentColor"
-                className="bi bi-box-arrow-up-right"
-                viewBox="0 0 16 16"
-              >
-                <path
-                  fillRule="evenodd"
-                  d="M8.636 3.5a.5.5 0 0 0-.5-.5H1.5A1.5 1.5 0 0 0 0 4.5v10A1.5 1.5 0 0 0 1.5 16h10a1.5 1.5 0 0 0 1.5-1.5V7.864a.5.5 0 0 0-1 0V14.5a.5.5 0 0 1-.5.5h-10a.5.5 0 0 1-.5-.5v-10a.5.5 0 0 1 .5-.5h6.636a.5.5 0 0 0 .5-.5"
-                />
-                <path
-                  fillRule="evenodd"
-                  d="M16 .5a.5.5 0 0 0-.5-.5h-5a.5.5 0 0 0 0 1h3.793L6.146 9.146a.5.5 0 1 0 .708.708L15 1.707V5.5a.5.5 0 0 0 1 0z"
-                />
-              </svg>;
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width={16}
+      height={16}
+      fill="currentColor"
+      className="bi bi-box-arrow-up-right inline"
+      viewBox="0 0 16 16"
+    >
+      <path
+        fillRule="evenodd"
+        d="M8.636 3.5a.5.5 0 0 0-.5-.5H1.5A1.5 1.5 0 0 0 0 4.5v10A1.5 1.5 0 0 0 1.5 16h10a1.5 1.5 0 0 0 1.5-1.5V7.864a.5.5 0 0 0-1 0V14.5a.5.5 0 0 1-.5.5h-10a.5.5 0 0 1-.5-.5v-10a.5.5 0 0 1 .5-.5h6.636a.5.5 0 0 0 .5-.5"
+      />
+      <path
+        fillRule="evenodd"
+        d="M16 .5a.5.5 0 0 0-.5-.5h-5a.5.5 0 0 0 0 1h3.793L6.146 9.146a.5.5 0 1 0 .708.708L15 1.707V5.5a.5.5 0 0 0 1 0z"
+      />
+    </svg>
+  );
 }
 
 async function Repos({ repo_option, uID }) {
@@ -49,20 +51,23 @@ async function Repos({ repo_option, uID }) {
       <h1 className="text-[22px] md:text-[26px]">{text}</h1>
       <br />
       {repos.map((repo, index) => (
-        <div key={index} className=" md:bg-black md:mb-2 md:pl-3 md:pr-3 md:pt-1 md:rounded-2xl ">
-          <p key={index}>
-            <a
-              href={repo[2]}
-              target="_blank"
-              rel="noopener noreferrer"
-              className="inline"
-            >
+        <div
+          key={index}
+          className=" bg-black mb-2 pl-3 pr-3 pt-1 rounded-2xl "
+        >
+          <a
+            href={repo[2]}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="inline"
+          >
+            <p key={index} className="inline-flex items-center gap-1">
               {repo[0]}
               <RedirectSVG></RedirectSVG>
-            </a>
-          </p>
-          <p>{repo[1]}</p>
-          <br></br>
+            </p>
+            <p>{repo[1]}</p>
+            <br></br>
+          </a>
         </div>
       ))}
     </ul>
@@ -116,15 +121,15 @@ export default async function Page({ params }) {
         backgroundColor: userData.bg_color,
       }}
     >
-      <div className="w-full max-w-6xl grid grid-cols-1 md:grid-cols-2 gap-12 items-start md:items-center
+      <div
+        className="w-full max-w-6xl grid grid-cols-1 md:grid-cols-2 gap-12 items-start md:items-center
       
       bg-[linear-gradient(to_top,#232526,#2b2d2e)] rounded-md p-[2em] text-white border-[#a8afb5] border-solid border-2 [box-shadow:0_0_6px_#a8afb5] "
-       // bg-[linear-gradient(to_top,#232526,#2b2d2e)] rounded-md p-[2em] text-white border-[#a8afb5] border-solid border-2 [box-shadow:0_0_6px_#a8afb5] darkmode
-       // bg-[linear-gradient(90deg,rgba(121,130,133,1)_0%,rgba(204,203,177,1)_100%)] rounded-lg p-[2em] text-black border-[#3f4042] border-solid border-2 [box-shadow:0_0_8px_#353738] lightmode
+        // bg-[linear-gradient(to_top,#232526,#2b2d2e)] rounded-md p-[2em] text-white border-[#a8afb5] border-solid border-2 [box-shadow:0_0_6px_#a8afb5] darkmode
+        // bg-[linear-gradient(90deg,rgba(121,130,133,1)_0%,rgba(204,203,177,1)_100%)] rounded-lg p-[2em] text-black border-[#3f4042] border-solid border-2 [box-shadow:0_0_8px_#353738] lightmode
       >
         {/* Left Column: Profile Info */}
         <div className="flex flex-col items-center text-center md:text-center md:justify-center md:items-center">
-          
           <Image
             src={userData.avatar_url}
             alt="User Avatar"
