f5bf64852d7faa2ca11f94ff62642ecc6e9d7806
swarties <76826079+swarties@users.noreply.github.com>
2025-12-05 21:51:24 -0500

    metadata fix #2

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,27 +1,5 @@
 import "./globals.css";
 
-/* export const metadata = {
-  title: "Codefolio — GitHub Portfolio",
-  description:
-    "A clean and minimalistic platform where developers can showcase their work. Link your GitHub account, generate a profile page, and share your code with the world.",
-  openGraph: {
-    title: "Codefolio — GitHub Portfolio",
-    description:
-      "A clean and minimalistic platform where developers can showcase their work. Link your GitHub account, generate a profile page, and share your code with the world.",
-    url: "https://codefolio.app",
-    siteName: "Codefolio",
-    images: [
-      {
-        url: "./opengraph-image.png", // Must be an absolute URL
-        width: 720,
-        height: 378,
-        alt: "Homepage Render",
-      },
-    ],
-    type: "website",
-  },
-}; */
-
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
