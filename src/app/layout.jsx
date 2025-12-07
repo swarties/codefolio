298f0847eb1ed4a9e44635685f9fb4dc1395e7c0
swarties <76826079+swarties@users.noreply.github.com>
2025-12-06 19:08:38 -0500

    og and twitter image metadata tag removal for nextjs file convention

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -22,18 +22,6 @@ export default function RootLayout({ children }) {
         <meta property="og:url" content="https://codefolio.app" />
         <meta property="og:site_name" content="Codefolio" />
         <meta property="og:type" content="website" />
-
-        <meta
-          property="og:image"
-          content="https://codefolio.app/opengraph-image.png"
-        />
-        <meta
-          property="og:image:secure_url"
-          content="https://codefolio.app/opengraph-image.png"
-        />
-        <meta property="og:image:type" content="image/png" />
-        <meta property="og:image:width" content="720" />
-        <meta property="og:image:height" content="378" />
         <meta property="og:image:alt" content="Homepage Render" />
 
         <meta name="twitter:card" content="summary_large_image" />
@@ -41,10 +29,6 @@ export default function RootLayout({ children }) {
         <meta
           name="twitter:description"
           content="A clean and minimalistic platform where developers can showcase their work. Link your GitHub account, generate a profile page, and share your code with the world."
-        />
-        <meta
-          name="twitter:image"
-          content="https://codefolio.app/opengraph-image.png"
         />
         <meta name="twitter:image:alt" content="Homepage Render" />
       </head>
